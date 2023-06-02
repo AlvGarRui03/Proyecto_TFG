c95ad6fa36c12c784f398255b81e251499489ff4
Alvaro <correo@ejemplo.com>
2023-06-02 10:23:05 +0200

    Added Imputado and Person Activities

--- app/src/main/java/com/example/aplicacinjuzgadotfg/ImputadoActivity.java
+++ app/src/main/java/com/example/aplicacinjuzgadotfg/ImputadoActivity.java
@@ -1,14 +1,24 @@
 package com.example.aplicacinjuzgadotfg;
 
+import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class ImputadoActivity extends AppCompatActivity {
-
+    private Context contexto = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imputado);
+        int nightModeFlags = contexto.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        switch (nightModeFlags) {
+            case Configuration.UI_MODE_NIGHT_YES:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                break;
+        }
+
     }
 }
--- app/src/main/java/com/example/aplicacinjuzgadotfg/PersonActivity.java
+++ app/src/main/java/com/example/aplicacinjuzgadotfg/PersonActivity.java
@@ -11,4 +11,5 @@ public class PersonActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person);
     }
+
 }
